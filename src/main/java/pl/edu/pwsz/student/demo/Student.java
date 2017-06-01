1e7590877f9b582defc600bb728d6bafdc47ba2e
Qboons <jakkoz91@gmial.com>
2017-06-01 18:06:20 +0200

    commiting changes

--- src/main/java/pl/edu/pwsz/student/demo/Student.java
+++ src/main/java/pl/edu/pwsz/student/demo/Student.java
@@ -4,5 +4,31 @@ package pl.edu.pwsz.student.demo;
  * Created by Student on 2017-05-30.
  */
 public class Student {
-    int year
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    int yearOfBirth;
+    String name;
+    String lastName;
 }
